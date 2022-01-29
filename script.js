df36e28c3772b73de04ccf5dedc1abe6db6646ad
arttminie <parktaehyung49@gmail.com>
2022-01-29 15:29:16 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1
+
+// // for(number = 1; number <= 10; number++){
+//     console.log(number)
+// }
+
+// 2
+
+// let num = 10
+
+// while( num >= 1){
+//    console.log(num)
+//   num = num - 1  
+// }
+
+// 3
+
+// for(x = 1; x <= 100; x+=2){
+//     console.log(x)
+// }
+
+// 4
+
+// for(y = 0; y <= 100; y+=2){
+//    console.log(y)
+// }
